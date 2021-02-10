0554816b85c5cb2b992cd22a08acc5bedc4f18a7
Nick Anthony <nicholas.anthony@northwestern.edu>
2021-02-10 15:15:41 -0600

    fix ability to run from source and without setuptools_scm installed.

--- src/mpl_qt_viz/__init__.py
+++ src/mpl_qt_viz/__init__.py
@@ -10,8 +10,19 @@ Subpackages
    visualizers
 
 """
-from . import version
+import logging
+try:
+    from . import version
+    _versionStr = version.version
+except ImportError:  # When running from source the version.py file won't have been created by setuptools_scm
+    logging.getLogger(__name__).info("Failed to import `version.py`. Trying to import setuptools_scm")
+    try:
+        import setuptools_scm
+        _versionStr = setuptools_scm.get_version(root='../..', relative_to=__file__)
+    except ImportError:  # setuptools_scm
+        logging.getLogger(__name__).warn("Failed to import setuptools_scm. Using fallback version string.")
+        _versionStr = "x.x.x-dev"
 
 __author__ = 'Nick Anthony'
+__version__ = _versionStr
 
-__version__ = version.version
